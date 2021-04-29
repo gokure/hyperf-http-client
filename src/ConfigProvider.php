f3108543648b8370293d5b7058fa0f783ef5438f
Gang Wu <gokure@gmail.com>
2021-04-29 10:54:35 +0800

    Add ConfigProvider.php

---
+++ src/ConfigProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gokure\Http\Client;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
